bbf51c11dc212ddb6eb33e64a2b5668ae94d73b1
Felix Karg <84768645+felix-karg@users.noreply.github.com>
2026-07-14 10:18:49 +0200

    Change Docker image name from nanatwn to felixkarg

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'docker build -t felixkarg/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push nanatwn/demo-app:jma-2.0'
     }
