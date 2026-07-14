8a275f0cde981785759dffd676cd416e4a23232f
Felix Karg <84768645+felix-karg@users.noreply.github.com>
2026-07-14 10:53:22 +0200

    Fix string quotes in buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t $imageName .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push $imageName'
+        sh "docker push $imageName"
     }
 }
